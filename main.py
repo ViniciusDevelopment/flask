1dd151955fa80bdf11b5c7adaf80db81ca820cc0
NOTEBOOK-VINICI\VINICIUS DEVELOPMENT <vpires@sescto.com.br>
2023-03-05 17:52:17 -0300

    iii

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, Response, jsonify
 import os
 import json
-import mimetypes
-from itertools import combinations
-from itertools import combinations
+# import mimetypes
+# from itertools import combinations
+# from itertools import combinations
 # import numpy as np
-from pandas import DataFrame
-from IPython.display import display
-from statistics import variance
-from scipy.stats import t, f, norm
-import matplotlib.pyplot as plt
-from flask_cors import CORS
+# from pandas import DataFrame
+# from IPython.display import display
+# from statistics import variance
+# from scipy.stats import t, f, norm
+# import matplotlib.pyplot as plt
+# from flask_cors import CORS
 
 app = Flask(__name__)
 # CORS(app, resources={"*": {"origins": "*"}})
