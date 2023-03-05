8233fc8ee45a307e7e614258ae1216effee14207
NOTEBOOK-VINICI\VINICIUS DEVELOPMENT <vpires@sescto.com.br>
2023-03-05 17:21:26 -0300

    TESTES

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from flask import Flask, jsonify
 import os
+import json
+import mimetypes
+from flask import Flask, Response, jsonify
+from itertools import combinations
+from itertools import combinations
+import numpy as np
+from pandas import DataFrame
+from IPython.display import display
+from statistics import variance
+from scipy.stats import t, f, norm
+import matplotlib.pyplot as plt
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={"*": {"origins": "*"}})
+
+
+
+# from flask import Flask, jsonify
+# import os
+
+# app = Flask(__name__)
 
 
 @app.route('/')
